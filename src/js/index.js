17df86fbd4d069a174e160d07bf77b5fc5329a5b
nyarbouh <nyarbouh@me.com>
2019-08-26 22:45:47 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,17 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
+  var pronoun = ["He", "It", "You"];
+  var adj = ["My", "Big", "His"];
+  var noun = ["Car.com", "Hat.com", "Bottle.com"];
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        document.querySelector("#text").innerHTML +=
+          "<li>" + pronoun[i] + adj[j] + noun[k] + "</li>";
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
